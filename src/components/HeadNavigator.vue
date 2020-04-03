d9bb0a4becfbca8dfcb9b1717241343651d851b3
Rubén Bermejo Romero <mejoruben@gmail.com>
2020-04-03 20:26:34 +0200

    Started making the head of the page

---
+++ src/components/HeadNavigator.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav id="navigator" class="nav-extended">
+    <div id="navbarlol" class="nav-content teal">
+      <ul class="tabs tabs-transparent">
+        <li class="tab">
+          <a href="#start" id="startLink">Start</a>
+        </li>
+        <li class="tab">
+          <a href="#about" id="aboutLink">About</a>
+        </li>
+        <li class="tab">
+          <a href="#projects" id="projectsLink">Projects</a>
+        </li>
+        <li class="tab">
+          <a href id="cv">CV</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HeadNavigator"
+};
+</script>
+
+<style scoped>
+    #navbarlol {
+        width: 100%;
+        position: fixed;
+        top: 0;
+        z-index: 1;
+    }
+</style>
---
+++ src/components/StartComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="start">
+        <div id="header">
+            <img id="logo" alt="my ugly mug" src="./../assets/yo.png">
+            <div id="title">
+                <h3 class="white-text">Rubén Bermejo Romero</h3>
+                <h5 class="white-text">Técnico Superior en Desarrollo de Aplicaciones Multiplataforma</h5>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'StartComponent',
+}
+</script>
+
+<style scoped>
+    #start {
+        margin-top: 48px;
+    }
+
+    #header {
+        height: 600px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: row;
+        border: black 1px solid;
+        background: url('./../assets/background1.jpg') no-repeat center;
+    }
+
+    #header > * {
+        padding: 30px;
+    }
+</style>
